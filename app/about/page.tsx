ec275c0ffd9c79f824c504497f3dc8367db4ca92
Habib-bhai <habibahmed918131@gmail.com>
2024-10-18 19:07:51 +0500

    about section done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,17 +2,11 @@
 import {  useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Image from "next/image";
+import Link from "next/link";
+
 export default function AboutPage() {
     const [sliderImageLink, setSliderImageLink] = useState("/call-of-duty-mobile-hero-a.jpg")
-
-    const [scale1, setScale1] = useState(1);
-
-    const [scale2, setScale2] = useState(1)
-    const [scale3, setScale3] = useState(1)
-    
-
-
-
+ 
   return (
     <>
     <Navbar/>
@@ -38,7 +32,21 @@ export default function AboutPage() {
 
            <h1 className="text-5xl font-bold">Our Mission</h1>       
            <p className="h-[1px] w-screen bg-gray-300/50 mt-8"></p>
-           <p>At Activision, we strive to create the most iconic brands in gaming and entertainment. We’re driven by our mission to deliver unrivaled gaming experiences for the world to enjoy, together.  Home to iconic franchises like Call of Duty®, Crash Bandicoot™, and Tony Hawk’s™ Pro Skater™, we're a global leader in interactive entertainment. Our goal? Delight millions with innovative, fun, and engaging games. With a legacy of success, we're pushing boundaries with cutting-edge technology and inclusive practices. Join us in delivering unforgettable entertainment and seize the chance to level up your career.</p>
+           <p className="text-xl mt-8 px-5">At Activision, we strive to create the most iconic brands in gaming and entertainment. We’re driven by our mission to deliver unrivaled gaming experiences for the world to enjoy, together.  Home to iconic franchises like Call of Duty®, Crash Bandicoot™, and Tony Hawk’s™ Pro Skater™, we're a global leader in interactive entertainment. Our goal? Delight millions with innovative, fun, and engaging games. With a legacy of success, we're pushing boundaries with cutting-edge technology and inclusive practices. Join us in delivering unforgettable entertainment and seize the chance to level up your career.</p>
+
+           <h1 className="text-5xl font-bold mt-20">Activate Your Future</h1>
+           <p className="h-[1px] w-screen bg-gray-300/50 mt-8"></p>
+           <p className="text-xl mt-8 px-5">We unite our global player community with epic entertainment, focusing on three core pillars:</p>
+           <p className="text-xl mt-8 px-5">People - fostering talent and learning opportunities.</p>
+           <p className="text-xl mt-8 px-5">Innovation - consistently pushing to iterate and evolve.</p>
+           <p className="text-xl mt-8 px-5">Excellence - delivering high-quality games, consistently year in and year out.</p>
+           <p className="text-xl mt-8 px-5">Join us at Activision, where inclusivity thrives, and together we shape the future of entertainment for our players. Explore open roles now!</p>
+            <div className="w-screen flex justify-center items-center mt-20">
+           <Image src={"/atvi-values.png"} width={500} height={500} alt="ativi-values" className="w-[80%] h-[60%]"/>
+              </div>   
+
+
+              <Link href={"https://www.activision.com/company/diversity-inclusion#values"}> <button className="w-[30%] md:w-[15%] h-16 rounded-3xl  bg-blue-600/60 hover:bg-blue-600 transition-colors duration-150 ease-in-out border-[3px] border-blue-800 font-bold text-xl ">Learn More</button></Link>
        </div>           
     </>
   )
